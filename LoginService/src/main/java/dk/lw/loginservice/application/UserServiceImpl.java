e00d69955f78620c552bf6c4e2c0c663cc7f3eef
amalielandt <malie@hotmail.dk>
2021-12-28 15:49:06 +0100

    Errorhandling

--- LoginService/src/main/java/dk/lw/loginservice/application/UserServiceImpl.java
+++ LoginService/src/main/java/dk/lw/loginservice/application/UserServiceImpl.java
@@ -21,36 +21,43 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-        User user = userRepository.findOneByCpr(request.getCpr());
+        try {
+            User user = userRepository.findOneByCpr(request.getCpr());
 
-        if(user != null) {
-            if (user.validPassword(request.getPassword())) {
-                String token = user.generateToken();
-                LoginResponse response = LoginResponse.newBuilder().setToken(token).build();
-                responseObserver.onNext(response);
-                responseObserver.onCompleted();
+            if (user != null) {
+                if (user.validPassword(request.getPassword())) {
+                    String token = user.generateToken();
+                    LoginResponse response = LoginResponse.newBuilder().setToken(token).build();
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                }
+                Status status = Status.INVALID_ARGUMENT.withDescription("Wrong CPR or password");
+                responseObserver.onError(status.asRuntimeException());
             }
-            Status status = Status.INVALID_ARGUMENT.withDescription("Wrong CPR or password");
+            Status status = Status.NOT_FOUND.withDescription("User with CPR: " + request.getCpr() + " not found");
+            responseObserver.onError(status.asRuntimeException());
+
+        } catch (Exception ex) {
+            Status status = Status.INTERNAL.withDescription(ex.getMessage());
             responseObserver.onError(status.asRuntimeException());
         }
-        Status status = Status.NOT_FOUND.withDescription("User with cpr: " + request.getCpr() + " not found");
-        responseObserver.onError(status.asRuntimeException());
-
     }
 
     @Override
     public void getUser(UserRequest request, StreamObserver<UserResponse> responseObserver) {
         try {
             User user = userRepository.findOneByCpr(request.getCpr());
-            if(user != null) {
-                UserResponse response = createResponse(user);
+            if (user != null) {
+                UserResponse response = createResponse(user, responseObserver);
                 responseObserver.onNext(response);
                 responseObserver.onCompleted();
             }
-            throw new Exception("User with cpr: " + request.getCpr() + " not found");
+            Status status = Status.NOT_FOUND.withDescription("User with CPR: " + request.getCpr() + " not found");
+            responseObserver.onError(status.asRuntimeException());
 
         } catch (Exception ex) {
-            responseObserver.onError(Status.NOT_FOUND.getCause());
+            Status status = Status.INTERNAL.withDescription(ex.getMessage());
+            responseObserver.onError(status.asRuntimeException());
         }
     }
 
@@ -58,45 +65,55 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
     public void getUserById(GetUserRequest request, StreamObserver<UserResponse> responseObserver) {
         try {
             Optional<User> optUser = userRepository.findById(UUID.fromString(request.getId()));
+
             if(optUser.isPresent()) {
                 User user = optUser.get();
-                UserResponse response = createResponse(user);
+                UserResponse response = createResponse(user, responseObserver);
                 responseObserver.onNext(response);
                 responseObserver.onCompleted();
             }
-            throw new Exception("User with id: " + request.getId() + " not found");
+            Status status = Status.NOT_FOUND.withDescription("User with id: " + request.getId() + " not found");
+            responseObserver.onError(status.asRuntimeException());
 
         } catch (Exception ex) {
-            responseObserver.onError(Status.NOT_FOUND.getCause());
+            Status status = Status.INTERNAL.withDescription(ex.getMessage());
+            responseObserver.onError(status.asRuntimeException());
         }
     }
 
     @Override
     public void updateUser(UpdateUserRequest request, StreamObserver<UserResponse> responseObserver) {
-       try {
-           User user = userRepository.findOneByCpr(request.getCpr());
-            boolean valid = user.validPassword(request.getOldPassword());
-           if (user != null && valid) {
+        try {
+            User user = userRepository.findOneByCpr(request.getCpr());
 
-               user.setFullName(request.getFullName());
-               user.setEmail(request.getEmail());
-               user.setPhoneNumber(String.valueOf(request.getPhoneNumber()));
-               user.setPassword(request.getNewPassword());
-               user.setSalary(request.getSalary());
-               user.getAddress().setStreet(request.getAddress().getStreet());
-               user.getAddress().setNumber(request.getAddress().getNumber());
-               user.getAddress().setCity(request.getAddress().getCity());
-               user.getAddress().setZipcode(request.getAddress().getZipcode());
-               user = userRepository.save(user);
+            if (user != null) {
+                boolean valid = user.validPassword(request.getOldPassword());
+                if(valid) {
+                    user.setFullName(request.getFullName());
+                    user.setEmail(request.getEmail());
+                    user.setPhoneNumber(request.getPhoneNumber());
+                    user.setPassword(request.getNewPassword());
+                    user.setSalary(request.getSalary());
+                    user.getAddress().setStreet(request.getAddress().getStreet());
+                    user.getAddress().setNumber(request.getAddress().getNumber());
+                    user.getAddress().setCity(request.getAddress().getCity());
+                    user.getAddress().setZipcode(request.getAddress().getZipcode());
+                    user = userRepository.save(user);
 
-               UserResponse response = createResponse(user);
-               responseObserver.onNext(response);
-               responseObserver.onCompleted();
-           }
-           throw new Exception("User not found or invalid password");
-       } catch (Exception ex) {
-           responseObserver.onError(Status.NOT_FOUND.getCause());
-       }
+                    UserResponse response = createResponse(user, responseObserver);
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                }
+                Status status = Status.INVALID_ARGUMENT.withDescription("Invalid password or CPR");
+                responseObserver.onError(status.asRuntimeException());
+            }
+            Status status = Status.NOT_FOUND.withDescription("User with CPR: " + request.getCpr() + " not found");
+            responseObserver.onError(status.asRuntimeException());
+
+        } catch (Exception ex) {
+            Status status = Status.INTERNAL.withDescription(ex.getMessage());
+            responseObserver.onError(status.asRuntimeException());
+        }
     }
 
     @Override
@@ -105,16 +122,17 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
             User user = new User(request);
             user = userRepository.save(user);
 
-            UserResponse response = createResponse(user);
+            UserResponse response = createResponse(user,responseObserver);
             responseObserver.onNext(response);
             responseObserver.onCompleted();
 
         } catch (Exception ex) {
-            responseObserver.onError(Status.ALREADY_EXISTS.getCause());
+            Status status = Status.INTERNAL.withDescription(ex.getMessage());
+            responseObserver.onError(status.asRuntimeException());
         }
     }
 
-    private UserResponse createResponse(User user) throws ParseException {
+    private UserResponse createResponse(User user, StreamObserver<UserResponse> responseObserver) throws ParseException {
         Address address = Address.newBuilder()
                 .setNumber(user.getAddress().getNumber())
                 .setStreet(user.getAddress().getStreet())
@@ -122,15 +140,15 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
                 .setZipcode(user.getAddress().getZipcode()).build();
 
         UserResponse response = UserResponse.newBuilder()
-                .setId(user.getId().toString())
-                .setType(stubs.user.Type.valueOf(user.getType().toString()))
-                .setCpr(user.getCpr())
-                .setEmail(user.getEmail())
-                .setFullName(user.getFullName())
-                .setPhoneNumber(user.getPhoneNumber())
-                .setSalary(user.getSalary())
-                .setAge(user.getAge())
-                .setAddress(address).build();
+                    .setId(user.getId().toString())
+                    .setType(stubs.user.Type.valueOf(user.getType().toString()))
+                    .setCpr(user.getCpr())
+                    .setEmail(user.getEmail())
+                    .setFullName(user.getFullName())
+                    .setPhoneNumber(user.getPhoneNumber())
+                    .setSalary(user.getSalary())
+                    .setAge(user.getAge())
+                    .setAddress(address).build();
 
         return response;
     }
